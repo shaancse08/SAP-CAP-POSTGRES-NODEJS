5a05a9db037115a54a30bda82de506513bb7fd01
avikinkitsolutions <avik.saha0104@hotmail.com>
2023-08-19 15:53:52 +0530

    .sh added

--- pg-build.sh
+++ pg-build.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
 mkdir -p gen/pg/db
-cds compile * > gen/pg/db/csn.json
+# cp -r db/data gen/pg/db
+cds compile '*' > gen/pg/db/csn.json
 cp pg-package.json gen/pg/package.json
-cp package-lock.json gen/pg/package-lock.json
+cp pg-package-lock.json gen/pg/package-lock.json
